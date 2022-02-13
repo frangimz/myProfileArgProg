0d4c1aa26f796e84086e49824b2f8df9157a15d6
frangimz <frangimz@gmail.com>
2022-02-13 17:55:18 -0300

    Creacion de todos los componentes a usar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { AptitudesComponent } from './components/aptitudes/aptitudes.component';
+import { ExperienciaYEducacionComponent } from './components/experiencia-y-educacion/experiencia-y-educacion.component';
+import { LogrosComponent } from './components/logros/logros.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AcercaDeComponent,
+    AptitudesComponent,
+    ExperienciaYEducacionComponent,
+    LogrosComponent
   ],
   imports: [
     BrowserModule
