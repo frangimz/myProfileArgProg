46779838ad7a4a04591a326237eeebeb32eeeb39
frangimz <frangimz@gmail.com>
2022-07-28 02:48:34 -0300

    agregue link heroku (backend) a los servicios

--- src/app/services/s-educacion.service.ts
+++ src/app/services/s-educacion.service.ts
@@ -7,7 +7,7 @@ import { Educacion } from '../model/educacion';
   providedIn: 'root'
 })
 export class SEducacionService {
-  eduURL = 'http://localhost:8080/educa/';
+  eduURL = 'https://myprofile-b.herokuapp.com/educa/';
 
   constructor(private httpClient: HttpClient) { }
 
--- src/app/services/s-experiencia.service.ts
+++ src/app/services/s-experiencia.service.ts
@@ -7,7 +7,7 @@ import { Experiencia } from '../model/experiencia';
   providedIn: 'root'
 })
 export class SExperienciaService {
-  expeURL = 'http://localhost:8080/explab/';
+  expeURL = 'https://myprofile-b.herokuapp.com/explab/';
 
   constructor(private httpClient: HttpClient) { }
 
